9b55e883f5c43b85f60a795658fb1fde3dd468a4
Saloni <146350330+saloni222444@users.noreply.github.com>
2024-08-28 23:16:54 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -8,7 +8,7 @@ public class Factorial{
     int factorial= 1;
     for(int i = 1; i<= number; i++)
       {
-           factorial*= i;
+           factorial = factorial*i;
       }
 
   System.out.println("FActorial of " + number + "is: " +factorial);
