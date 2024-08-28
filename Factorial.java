98278694646475a633ef73fe1392ef9ab167f408
Saloni <146350330+saloni222444@users.noreply.github.com>
2024-08-28 22:58:46 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,16 @@
+//factorial logic: multiplies a positive integer by all the positive integers less than it//
+import java.util.*;
+public class Factorial{
+  public static void main(String[] args){
+    System.out.print("Enter a number");
+    Scanner sc = new Scanner(System.in);
+    int number = sc.nextInt();
+    int factorial= 1;
+    for(int i = 1; i<= number; i++)
+      {
+           factorial*= i;
+      }
+
+  System.out.println("FActorial of " + number + "is: " +factorial);
+  }
+} 
