0c8376c5b5d68acf31a8d77c4a64b7b9c7465917
Saloni <146350330+saloni222444@users.noreply.github.com>
2024-12-27 10:46:56 +0530

    user input

--- 3.Array/array.java
+++ 3.Array/array.java
@@ -3,5 +3,15 @@ import java.util.*;
 public class array{
     public static void main(String args[]){
         int marks[] = new int[100];
+
+        Scanner sc = new Scanner(System.in);
+
+        marks[0] = sc.nextInt();
+        marks[1] = sc.nextInt();
+        marks[2] = sc.nextInt();
+
+        System.out.println("phy:" +marks[0]);
+        System.out.println("chemistry: " +marks[1]);
+        System.out.pritnln("Matn: " +marks[2]);
     }
 }
