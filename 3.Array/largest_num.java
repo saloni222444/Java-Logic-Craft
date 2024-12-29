d125b645577725a59014b971538c9e69bf0b3213
Saloni <146350330+saloni222444@users.noreply.github.com>
2024-12-29 15:37:13 +0530

    add code for smallest number also

--- 3.Array/largest_num.java
+++ 3.Array/largest_num.java
@@ -1,13 +1,18 @@
 
 public class largest_num{
-
     public static int get_largest(int number[]){
         int largest = Integer.MIN_VALUE;
+        int smallest = Integer.MAX_VALUE;
         for(int i=0; i<number.length; i++){
               if(largest < number[i]){
                   largest = number[i];
               }
+              if(smallest> number[i]) 
+              {
+                smallest = number[i];
+              }
         }
+        System.out.println("smallest number is "+smallest);
         return largest;
     }
     public static void main (String args[]){
