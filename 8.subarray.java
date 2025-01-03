26c2fe1d3d593f331c190cd5de05f789a29e19a9
Saloni <146350330+saloni222444@users.noreply.github.com>
2025-01-03 23:35:47 +0530

    write code for print subarray

---
+++ 8.subarray.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class subarray{
+    public static void Printsubarray(int number[]){
+        for(int i=0; i<number.length; i++){
+           int start = i;
+           for(int j=i; j<number.length; j++){
+              int end =  j;
+              for(int k=start; k<=end; k++){
+                System.out.print(number[k]+" ");
+              }
+              System.out.println();
+            }   
+             System.out.println(); 
+        }   
+    }
+     
+
+    public static void main(String args[]){
+        int number[] = {2, 4, 6, 8, 10};
+        Printsubarray(number);
+    }
+}
