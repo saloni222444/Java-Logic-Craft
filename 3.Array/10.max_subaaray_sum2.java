83767771769a4ddf749dbba717725832c974ea28
Saloni <146350330+saloni222444@users.noreply.github.com>
2025-01-04 23:47:20 +0530

    create file to write code for get maximum sum in subarray

---
+++ 3.Array/10.max_subaaray_sum2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class 10.max_subarray_sum{ 
+    public static void prefix_subarray_sum(int number[]){
+        
+    }
+    public static void main(String args[]){
+        int number[] = {1, 4, 6, 3, 5};
+    }
+}
