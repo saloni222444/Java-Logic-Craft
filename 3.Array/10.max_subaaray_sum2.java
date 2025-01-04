529159fb078b3e8a7c72b424df8cd9447f950ca1
Saloni <146350330+saloni222444@users.noreply.github.com>
2025-01-04 23:50:06 +0530

    add comment

--- 3.Array/10.max_subaaray_sum2.java
+++ 3.Array/10.max_subaaray_sum2.java
@@ -1,8 +1,16 @@
+// This code finds the maximum subarray sum by generating all possible subarrays,
+// calculating their sums, and identifying the highest sum.
+
+// Brute Force Method:
+// Use nested loops to generate all possible subarrays.
+// Calculate the sum of each subarray.
+// Compare and update the maximum sum (maxsum).
+
 import java.util.*;
 
 public class 10.max_subarray_sum{ 
     public static void prefix_subarray_sum(int number[]){
-        
+
     }
     public static void main(String args[]){
         int number[] = {1, 4, 6, 3, 5};
